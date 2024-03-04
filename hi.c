e97df49ffa379afd446cd3d3a6d0ca52c9589c87
Divyangna Moudgil <divyangna111105@gmail.com>
2024-03-04 15:23:26 +0530

    Second commit is only for .c file

--- hi.c
+++ hi.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int main()
 {
-  printf("Hello, this is the first printf statement!"); 
+  printf("Hello, this is the first printf statement!");
+  printf("Second printf statement for c file");
 }
